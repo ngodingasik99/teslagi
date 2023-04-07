227a64af08dff16753d7ab1a680212a3b2d64c55
ngodingasik99 <ngodingasik7@gmail.com>
2023-04-07 22:26:25 +0700

    dari master

--- resources/views/dashboard/sidebar.blade.php
+++ resources/views/dashboard/sidebar.blade.php
@@ -64,7 +64,7 @@
           <li class="nav-item">
             <a class="nav-link" href="/kategori/">
               <span data-feather="file"></span>
-              Kategori 
+              Kategori
             </a>
           </li>
           <li class="nav-item">
